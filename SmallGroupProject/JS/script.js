9b68901920a932cdfbafe17b9f882c6f50bdc1e5
Christian King <99133333+JavaNoTea@users.noreply.github.com>
2023-01-23 18:47:55 -0500

    Add files via upload

---
+++ SmallGroupProject/JS/script.js
@@ -0,0 +1,7 @@
+function move2login(){
+    document.getElementById('everything-container').style.transform = "translate(-25%)";
+}
+
+function move2register(){
+    document.getElementById('everything-container').style.transform = "translate(0%)";
+}
