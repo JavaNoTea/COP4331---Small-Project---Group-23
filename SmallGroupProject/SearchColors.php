20ec9066afe94514f44f1a9fd14243d9b04de9ac
Tayl-r <91978020+Tayl-r@users.noreply.github.com>
2023-01-25 11:56:30 -0500

    Add files via upload

---
+++ SmallGroupProject/SearchColors.php
@@ -0,0 +1,73 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	$searchResults = "";
+	$searchCount = 0;
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("select * from Contacts where Name like ? and UserID=?");
+		//$stmt = $conn->prepare("select * from Contacts where Name like ? and UserID=?");
+		$colorName = "%" . $inData["search"] . "%";
+		//$colorName = $inData["search"];
+		$stmt->bind_param("si", $colorName, $inData["UserID"]);
+		$stmt->execute();
+		
+		$result = $stmt->get_result();
+		
+		while($row = $result->fetch_assoc())
+		{
+			if( $searchCount > 0 )
+			{
+				$searchResults .= ",";
+			}
+			$searchCount++;
+			//$searchResults .= '"' . $row['firstName"] . '"';
+			$searchResults .= '{"Name" : "' . $row['Name']. '", "Phone" : "' . $row['Phone']. '", "Email" : "' . $row['Email']. '"}';
+			// , "lastName" : "' . $row["LastName"]. '", "Phone" : "' . $row["Phone"]. '", "Email" : "' . $row["Email"]. '
+		}
+		
+		if( $searchCount == 0 )
+		{
+			returnWithError( "No Records Found" );
+		}
+		else
+		{
+			returnWithInfo( $searchResults );
+		}
+		
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
